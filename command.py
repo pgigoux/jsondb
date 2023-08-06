deef3e2af699d652c4c424872d01ece4f5cd660d
pgigoux <pgigoux@gemini.edu>
2023-08-06 15:52:03 -0400

    Add create_database, update read_database

--- command.py
+++ command.py
@@ -8,36 +8,29 @@ class CommandProcessor:
         self.file_name = ''
         self.db = None
 
-    def read_database(self) -> bool:
-        """
-        Read database into memory
-        :return:
-        """
-        self.db = Database(self.file_name, get_password())
-        try:
-            self.db.read()
-            return True
-        except FileNotFoundError:
-            print(f'File {self.file_name} does not exist')
-        except ValueError:
-            print('Failed to read database')
-        return False
-
-    def no_database(self) -> bool:
+    def db_loaded(self) -> bool:
         """
         Check whether there's a database in memory
         :return: True if that's the case, False otherwise
         """
-        if self.db is None:
-            print('No database loaded')
-            return False
-        else:
-            return True
+        return self.db is None
 
-    def read_command(self, file_name=DEFAULT_DATABASE_NAME):
+    def create_database(self, file_name=DEFAULT_DATABASE_NAME):
+        """
+        Create an empty database
+        :param file_name: database file name
+        """
+        self.file_name = file_name
+        self.db = Database(file_name, get_password())
 
-        # Make sure we are not overwriting the database
-        if not self.no_database():
+    def read_database(self, file_name=DEFAULT_DATABASE_NAME):
+        """
+        Read database into memory
+        :param file_name: database file name
+        :return:
+        """
+        # Make sure we are not overwriting an existing database
+        if self.db_loaded():
             print('There is a database in memory already')
             answer = input('Do you want to overwrite it (yes/no)? ')
             if answer not in ['yes']:
@@ -48,7 +41,6 @@ class CommandProcessor:
         self.file_name = file_name
         try:
             self.db.read()
-            return
         except Exception as e:
             print('Failed to read database', repr(e))
 
