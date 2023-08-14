cfb309ebf9c6f094f3fc8ee82937379dd2e0e701
pgigoux <pgigoux@gemini.edu>
2023-08-14 18:35:43 -0400

    Add main command interpreter

---
+++ main.py
@@ -0,0 +1,43 @@
+from cmd import Cmd
+from parser import Parser
+
+QUIT_COMMAND = 'quit'
+
+
+class CommandInterpreter(Cmd):
+    prompt = 'cmd> '
+    intro = ''
+
+    def __init__(self, p: Parser):
+        super().__init__()
+        self.parser = p
+
+    # trap EOF (CTRL-D)
+    def do_EOF(self, _) -> bool:
+        self.parser.execute(QUIT_COMMAND)
+        return True
+
+    # ignore empty lines
+    def emptyline(self) -> bool:
+        return False
+
+    def do_help(self, topic: str):
+        pass
+
+    # Process command
+    def default(self, command: str):
+        self.parser.execute(command)
+
+    def do_bye(self, _: str) -> bool:
+        self.parser.execute(QUIT_COMMAND)
+        return True
+
+
+if __name__ == '__main__':
+    parser = Parser()
+    ci = CommandInterpreter(parser)
+    try:
+        ci.cmdloop()
+    except KeyboardInterrupt:
+        parser.execute(QUIT_COMMAND)
+        exit(0)
