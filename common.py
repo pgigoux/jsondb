b972f852ec279d174411020559a231ce095f95d0
pgigoux <pgigoux@gemini.edu>
2023-08-19 12:33:36 -0400

    Add KEY_COUNT

--- common.py
+++ common.py
@@ -5,6 +5,7 @@ Common definitions used in the code
 # Keys used to access the name and unique identifier in tables and database items
 KEY_NAME = 'name'
 KEY_UID = 'uid'
+KEY_COUNT = 'count'
 
 # Field attribute keys
 FIELD_NAME_KEY = KEY_NAME
