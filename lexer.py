84fcbd0dee50f484864c3b7dc78971bb4066635d
pgigoux <pgigoux@gemini.edu>
2023-09-22 14:52:54 -0300

    Add SW_FIELD_DELETE, rename State

--- lexer.py
+++ lexer.py
@@ -37,6 +37,7 @@ class Tid(Enum):
     SW_NAME = auto()
     SW_FIELD = auto()
     SW_FIELD_VALUE = auto()
+    SW_FIELD_DELETE = auto()
     SW_TAG = auto()
     SW_NOTE = auto()
     SW_NOTE_TEXT = auto()  # multiline note
@@ -45,7 +46,7 @@ class Tid(Enum):
 
 
 # Lexer DFA states
-class State(Enum):
+class LexState(Enum):
     START = auto()
     WORD = auto()
     STRING = auto()
@@ -109,7 +110,7 @@ class Lexer:
         self.command = ''
         self.count = 0
         self.char_list = []
-        self.state = State.START
+        self.state = LexState.START
         self.keywords = {
             'item': Tid.ITEM, 'field': Tid.FIELD, 'tag': Tid.TAG,
             'new': Tid.NEW, 'read': Tid.READ, 'write': Tid.WRITE,
@@ -126,6 +127,7 @@ class Lexer:
             '-t': Tid.SW_TAG,
             '-f': Tid.SW_FIELD,
             '-fn': Tid.SW_FIELD,
+            '-fd': Tid.SW_FIELD_DELETE,
             '-fv': Tid.SW_FIELD_VALUE,
             '-note': Tid.SW_NOTE,
             '-text': Tid.SW_NOTE_TEXT
@@ -139,36 +141,36 @@ class Lexer:
         # the trailing space is needed by the state machine to parse properly
         self.command = command.strip()
         self.char_list = list(self.command + ' ')
-        self.state = State.START
+        self.state = LexState.START
         self.count = 0
 
-    def token(self, word: str) -> Token:
+    def token(self, pattern: str) -> Token:
         """
         Check for matching patterns and return token code and data.
         Keywords are always checked first.
         The order patterns are checked matters.
-        :param word:
-        :return: TODO
+        :param pattern: pattern to chek
+        :return: Token
         """
-        if word in self.keywords:
-            tup = Token(self.keywords[word], word)
-        elif word in self.switches:
-            tup = Token(self.switches[word], True)
-        elif re.search(LONG_DATE_PATTERN, word) \
-                or re.search(SHORT_DATE_PATTERN, word) \
-                or re.search(MONTH_YEAR_PATTERN, word):
-            tup = Token(Tid.VALUE, word)
-        elif re.search(FLOAT_PATTERN, word):
-            tup = Token(Tid.VALUE, float(word))
-        elif re.search(INT_PATTERN, word):
-            tup = Token(Tid.VALUE, int(word))
-        elif re.search(FILE_PATTERN, word):
-            tup = Token(Tid.FILE, word)
-        elif re.search(NAME_PATTERN, word):
-            tup = Token(Tid.NAME, word)
+        if pattern in self.keywords:
+            t = Token(self.keywords[pattern], pattern)
+        elif pattern in self.switches:
+            t = Token(self.switches[pattern], True)
+        elif re.search(LONG_DATE_PATTERN, pattern) \
+                or re.search(SHORT_DATE_PATTERN, pattern) \
+                or re.search(MONTH_YEAR_PATTERN, pattern):
+            t = Token(Tid.VALUE, pattern)
+        elif re.search(FLOAT_PATTERN, pattern):
+            t = Token(Tid.VALUE, float(pattern))
+        elif re.search(INT_PATTERN, pattern):
+            t = Token(Tid.VALUE, int(pattern))
+        elif re.search(FILE_PATTERN, pattern):
+            t = Token(Tid.FILE, pattern)
+        elif re.search(NAME_PATTERN, pattern):
+            t = Token(Tid.NAME, pattern)
         else:
-            tup = Token(Tid.INVALID, word)
-        return tup
+            t = Token(Tid.INVALID, pattern)
+        return t
 
     def next_token(self) -> Token:
         """
@@ -180,30 +182,30 @@ class Lexer:
             c = self.char_list[self.count]
             self.count += 1
             assert isinstance(c, str)
-            if self.state == State.START:
+            if self.state == LexState.START:
                 if c.isspace():
                     pass
                 elif c in STRING_DELIMITERS:
-                    self.state = State.STRING  # start of string
+                    self.state = LexState.STRING  # start of string
                     word = ''
                 else:
                     word = c
-                    self.state = State.WORD  # start of word
-            elif self.state == State.WORD:
+                    self.state = LexState.WORD  # start of word
+            elif self.state == LexState.WORD:
                 if c.isspace():
-                    self.state = State.START
+                    self.state = LexState.START
                     return self.token(word)  # end of word
                 else:
                     word += c
-            elif self.state == State.STRING:
+            elif self.state == LexState.STRING:
                 if c in STRING_DELIMITERS:
-                    self.state = State.START
+                    self.state = LexState.START
                     return Token(Tid.STRING, word)  # end of string
                 else:
                     word += c
 
         # Check for unterminated string
-        if self.state == State.STRING:
+        if self.state == LexState.STRING:
             return Token(Tid.INVALID, f'{UNTERMINATED_STRING} [{word[0:10]}...]')
         else:
             return Token(Tid.EOS, '')
